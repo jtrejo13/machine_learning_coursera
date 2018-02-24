7ba14456689dfd9ea398810eeea8a3276462602a
jtrejo13 <jtrejo13@utexas.edu>
2018-02-24 09:52:32 -0600

    Implement logistic regression

--- coding_assignments/02_logistic_regression/code/costFunctionReg.m
+++ coding_assignments/02_logistic_regression/code/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h_theta = sigmoid(X*theta);
 
+J = 1 / m * sum(-y.*log(h_theta) - (1 - y).*log(1 - h_theta)) ...
+          + lambda / (2 * m) * sum(theta(2:end).^2); % Regularization term
 
-
-
+grad = 1 / m * X'*(h_theta - y);
+grad(2:end) = grad(2:end) + lambda / m .* theta(2:end);
 
 % =============================================================
 
