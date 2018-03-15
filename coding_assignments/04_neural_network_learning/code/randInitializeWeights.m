d7c767dc0f40365cb857d773ace3978562a744dc
jtrejo13 <jtrejo13@utexas.edu>
2018-03-15 15:19:00 -0500

    Finish neural network learning

--- coding_assignments/04_neural_network_learning/code/randInitializeWeights.m
+++ coding_assignments/04_neural_network_learning/code/randInitializeWeights.m
@@ -19,13 +19,7 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
-
-
-
-
-
-
-
+W = -0.12 + (0.24)*rand(size(W));
 
 % =========================================================================
 
